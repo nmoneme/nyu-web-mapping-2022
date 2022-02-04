2c2e3cc24f4d733056bbb4c740776c82155abd1c
nmoneme <98977715+nmoneme@users.noreply.github.com>
2022-02-03 19:24:26 -0500

    fist commit after porting jsfiddle to separate files

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$('#term-1').on('click', function() {
+	$(".right-half").text("A request by you for payment by the insurance company of medical expenses that are covered under the insurance policy. The provider of a medical service will usually file a claim for you.");
+})
+
+$('#term-2').on('click', function() {
+	$(".right-half").text("A system under which individuals, businesses and other organizations, in exchange for a premium, are promised payment for losses resulting from certain dangers as specified in a contract.");
+})
+
+$('#term-3').on('click', function() {
+	$(".right-half").text("The total you pay out of your pocket for a policy year. These costs include the deductible, co-insurance and amounts considered by the insurance company to be above the Usual and Customary charges.");
+})
